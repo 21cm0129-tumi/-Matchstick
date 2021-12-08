fd94c2be9173fa05b2477009605dd2fde91b0514
21cm0129-tumi <21cm0129@jec.ac.jp>
2021-12-09 02:13:20 +0900

    Result画面のサンプル

---
+++ MatchStick/MatchStick/MatchStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchStickApp.swift
+//  MatchStick
+//
+//  Created by cmStudent on 2021/12/05.
+//
+
+import SwiftUI
+
+@main
+struct MatchStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ButtleView()
+        }
+    }
+}
